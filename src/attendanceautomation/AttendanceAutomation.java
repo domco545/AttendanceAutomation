95abb4683b43c2e2d853a081790d0430c58c7ade
Dominik Rothbauer <dominikrothbauer@gmail.com>
2020-02-19 09:31:02 +0100

    fixed path bugs

--- src/attendanceautomation/AttendanceAutomation.java
+++ src/attendanceautomation/AttendanceAutomation.java
@@ -19,7 +19,7 @@ public class AttendanceAutomation extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/gui/view/Main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("gui/view/Main.fxml"));
         
         Scene scene = new Scene(root);
         
